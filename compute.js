caf8baf0d8714dd041f016f2ec4915f61ceb76c4
Sravan46 <S546947@nwmissouri.edu>
2022-03-30 23:18:01 -0500

    updated

---
+++ compute.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var value;
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    fake_url = "https://fake.com/path" + req.url
+      const url = new URL(fake_url)
+      const search_params = url.searchParams
+     value =Number(search_params.get("x"))
+    if(value == 0)
+    value=Math.random()
+    res.write("Computes the values for Math.atan and math.cbrt function."+"\n")
+    res.write('Math.log2() applied to '+value+" is "+Math.log2(value)+"\n");
+    res.write('Math.cosh() applied to '+value+" is "+Math.cosh(value)+"\n");
+    res.end('Math.floor() applied to '+value+" is "+Math.floor(value));
+ });
+
+module.exports = router;
